d12ed028226fc57a25094ff7bc826be20c102ea3
Asgar <asgarabbas532@gmail.com>
2023-02-08 23:52:20 +0530

    .

---
+++ app/src/main/java/com/asgar72/activityintent/Activitys/Activity.java
@@ -0,0 +1,16 @@
+package com.asgar72.activityintent.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.asgar72.activityintent.R;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_);
+    }
+}
